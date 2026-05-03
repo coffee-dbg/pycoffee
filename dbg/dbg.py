d86482977855b0a06e6f2c44d867b5d2b0de0188
coffee-dbg <coffee.code.src@gmail.com>
2026-05-03 16:56:15 +0200

    [IMP] dbg: manage stepping according to frames

--- dbg/dbg.py
+++ dbg/dbg.py
@@ -1,4 +1,5 @@
 import contextlib
+import linecache
 import logging
 import os
 import shlex
@@ -7,7 +8,7 @@ import sysconfig
 import threading
 from argparse import ArgumentError, ArgumentParser
 from collections import defaultdict
-from types import CodeType
+from types import CodeType, FrameType
 from pathlib import Path
 
 STD_LIB = Path(sysconfig.get_paths()['stdlib'])
@@ -19,15 +20,91 @@ TOOL_ID = 4  # sys.monitoring.DEBUGGER_ID
 TOOL_NAME = 'PYCOFFEE_TOOL'
 
 RUNNING_SCRIPT = threading.Event()
-RUNNING_SCRIPT.clear()
+RUNNING_SCRIPT.set()
 RUNNING_DBG = threading.Event()
-RUNNING_DBG.set()
+RUNNING_DBG.clear()
 TID_DBG = None
 
 _logger = logging.getLogger(__name__)
 logging.basicConfig(
     format="%(asctime)s [%(name)s] %(message)s"
 )
+
+
+class Breakpoints:
+
+    # TODO: implement permanent breakpoints
+
+    def __init__(self):
+        self.breakpoints = defaultdict(dict)
+
+    def add(self, filename: str, line_number: int | None = None):
+        filename_breakpoints = self.breakpoints[filename]
+        filename_breakpoints[line_number] = {}
+
+    def get(self, filename: str, line_number: int) -> dict | None:
+        filename_breakpoints = self.breakpoints[filename]
+        try:
+            return filename_breakpoints.pop(line_number)
+        except KeyError:
+            return filename_breakpoints.pop(None, None)
+
+
+class ThreadState:
+
+    _frame: FrameType | None
+    _step_frame: FrameType | None
+
+    def __init__(self):
+        self._frame = None
+        self._lineno = None
+        self._step_reset()
+        self.step_over = lambda: self._step('over')
+        self.step_into = lambda: self._step('into')
+        self.step_out = lambda: self._step('out')
+
+    def _step_reset(self):
+        self._step_mode = None
+        self._step_frame = None
+        self._step_lineno = None
+
+    def __repr__(self):
+        return f'ThreadState(mode={self._step_mode}, frame={id(self._frame)}, step_frame={id(self._step_frame)})'
+
+    def update(self, frame: FrameType):
+        self._frame = frame
+        self._lineno = frame.f_lineno
+
+    def _step(self, mode: str):
+        self._step_mode = mode
+        self._step_frame = self._frame
+        self._step_lineno = self._lineno
+
+    def _is_step_ancestor(self) -> bool:
+        f = self._step_frame.f_back
+        while f:
+            if f is self._frame:
+                return True
+            f = f.f_back
+        return False
+
+    def must_break(self) -> bool:
+        stop = False
+        match self._step_mode:
+            case 'over':
+                if (self._frame is self._step_frame and self._lineno != self._step_lineno) or self._is_step_ancestor():
+                    stop = True
+            case 'into':
+                if self._frame is not self._step_frame or self._lineno != self._step_lineno:
+                    stop = True
+            case 'out':
+                if self._is_step_ancestor():
+                    stop = True
+            case _:
+                return False
+        if stop:
+            self._step_reset()
+        return stop
 
 
 class DBG:
@@ -65,15 +142,14 @@ class DBG:
         sys.path[0] = str(script_path.parent)
         sys.argv[:] = script_args
 
-        self.thread_states = defaultdict(lambda: {
-            'step_depth': False,
-            'depth': 0,
-        })
-
-        self.current_tid = None
+        self.thread_states = defaultdict(ThreadState)
 
         dbg_thread = threading.Thread(target=self.repl, name='<DBG (REPL)>')
         dbg_thread.start()
+
+        # Add first breakpoint
+        self.breakpoints = Breakpoints()
+        self.breakpoints.add(code.co_filename)
 
         with self.monitoring():
             exec(code, globals_)
@@ -85,28 +161,33 @@ class DBG:
     def monitoring(self):
         MONITORING.use_tool_id(TOOL_ID, TOOL_NAME)
         MONITORING.register_callback(TOOL_ID, EVENTS.LINE, self.monitoring_callback_line)
-        MONITORING.register_callback(TOOL_ID, EVENTS.CALL, self.monitoring_callback_call)
-        MONITORING.register_callback(TOOL_ID, EVENTS.PY_RETURN, self.monitoring_callback_return)
-        MONITORING.set_events(TOOL_ID, EVENTS.LINE | EVENTS.CALL | EVENTS.PY_RETURN)
+        MONITORING.set_events(TOOL_ID, EVENTS.LINE)
         try:
             yield
         finally:
             MONITORING.set_events(TOOL_ID, 0)
             MONITORING.free_tool_id(TOOL_ID)
 
-    def monitoring_callback_call(self, code: CodeType, instruction_offset: int, callable: object, arg0: object):
-        if ((tid := threading.get_ident()) == self.dbg_tid): return
-        RUNNING_SCRIPT.wait()
-        self.thread_states[tid]['depth'] += 1
-
-    def monitoring_callback_return(self, code: CodeType, instruction_offset: int, retval: object):
-        if ((tid := threading.get_ident()) == self.dbg_tid): return
-        RUNNING_SCRIPT.wait()
-        self.thread_states[tid]['depth'] -= 1
-
     def monitoring_callback_line(self, code: CodeType, line_number: int):
-        if ((tid := threading.get_ident()) == self.dbg_tid): return
-        RUNNING_SCRIPT.wait()
+        tid = threading.get_ident()
+
+        if (tid == self.dbg_tid): return
+
+        state = self.thread_states[tid]
+        state.update(sys._getframe(1))
+
+        RUNNING_SCRIPT.wait()  # Wait after update
+
+        # Check break
+        if (
+            (breakpoint := self.breakpoints.get(code.co_filename, line_number)) is not None
+            or state.must_break()
+        ):
+
+            self.selected_tid = tid
+            RUNNING_SCRIPT.clear()
+            RUNNING_DBG.set()
+            RUNNING_SCRIPT.wait()  # To prevent next line of the debugged thread
 
     def repl(self):
         self.dbg_tid = threading.get_ident()
@@ -165,29 +246,36 @@ class DBG:
 
     @cmd('thread', thread_parser, alias='t')
     def _thread(self, args):
-        print(f'Thread:', threading.get_ident())
+        print(f'Current Thread:', threading.get_ident())
+        print(f'Selected Thread:', self.selected_tid)
         if args.list:
+            frames = sys._current_frames()
             for t in threading.enumerate():
-                print(f'- name={t.name} | id={t.ident}')
+                print(f'- {t.name} ({t.ident}) [alive: {t.is_alive()}]')
+
+    @cmd('line', ArgumentParser(), alias='l')
+    def _line(self, args):
+        code = self.thread_states[self.selected_tid]._frame.f_code
+        line_number = self.thread_states[self.selected_tid]._frame.f_lineno
+        filename = code.co_filename
+        line = linecache.getline(filename, line_number).rstrip()
+        if line:
+            print(f'<{filename}:{line_number}> -> {line}')
 
     @cmd('step_over', ArgumentParser(), alias='s', stop=True)
     def _step_over(self, args):
         print('step over')
-        thread_state = self.thread_states[self.current_tid]
-        thread_state['step_depth'] = thread_state['depth']
-        print(self.tid, thread_state['step_depth'])
+        self.thread_states[self.selected_tid].step_over()
 
     @cmd('step_into', ArgumentParser(), alias='i', stop=True)
     def _step_into(self, args):
         print('step into')
-        thread_state = self.thread_states[self.current_tid]
-        thread_state['step_depth'] = thread_state['depth'] + 1
+        self.thread_states[self.selected_tid].step_into()
 
     @cmd('step_out', ArgumentParser(), alias='o', stop=True)
     def _step_out(self, args):
         print('step out')
-        thread_state = self.thread_states[self.current_tid]
-        thread_state['step_depth'] = thread_state['depth'] - 1
+        self.thread_states[self.selected_tid].step_out()
 
     @cmd('continue', ArgumentParser(), alias='c', stop=True)
     def _continue(self, args):
